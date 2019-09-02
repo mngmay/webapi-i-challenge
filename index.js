9759d6d6c69da859b102e8e0b2ea4d2443cd1380
May Ng <mngmay@gmail.com>
2019-09-02 14:31:48 -0700

    Downloaded dependencies. Set up port. Get request confirms it's up and running

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // implement your API here
+
+const express = require("express");
+
+const Users = require("./data/db.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send("You got the server");
+});
+
+const port = 3000;
+server.listen(port, () => console.log("\napi running\n"));
