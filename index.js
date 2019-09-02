cc1bc2152285d316e50a7279900eee34a45ee68a
May Ng <mngmay@gmail.com>
2019-09-02 15:26:55 -0700

    Updated DELETE and PUT based on endpoint specifications

--- index.js
+++ index.js
@@ -69,10 +69,16 @@ server.delete("/api/users/:id", (req, res) => {
 
   Users.remove(userId)
     .then(user => {
-      res.status(200).json({ message: "user deleted successfully" });
+      if (user) {
+        res.status(200).json({ message: "user deleted successfully" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist" });
+      }
     })
     .catch(error => {
-      res.status(500).json({ message: "error deleting the user" });
+      res.status(500).json({ message: "The user could not be removed" });
     });
 });
 
@@ -80,16 +86,25 @@ server.put("/api/users/:id", (req, res) => {
   const id = req.params.id;
   const changes = req.body;
 
+  if (!changes.name || !changes.body) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user" });
+  }
   Users.update(id, changes)
     .then(updated => {
       if (updated) {
         res.status(200).json(updated);
       } else {
-        res.status(404).json({ message: "user not found" });
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
       }
     })
     .catch(error => {
-      res.status(500).json({ message: "error updating user" });
+      res
+        .status(500)
+        .json({ message: "The user information could not be modified." });
     });
 });
 
